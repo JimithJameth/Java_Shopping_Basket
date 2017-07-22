f391ff3f8516aca71f6a322dbeb69950416edab9
Jamie Martin <jamiemartinmusic@gmail.com>
2017-07-22 18:07:28 +0100

    Item has type test passed

--- shopping_basket_specs/ItemTest.java
+++ shopping_basket_specs/ItemTest.java
@@ -22,4 +22,9 @@ public class ItemTest {
   public void testItemHasPrice(){
     assertEquals(1.99, item1.getPrice(),.01);
   }
+
+  @Test
+  public void testItemHasType(){
+    assertEquals(Type.FRESHFOOD,item1.getType());
+  }
 }
