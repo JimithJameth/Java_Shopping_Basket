eb80af2b37381110f071113c1babc820adab896a
Jamie Martin <jamiemartinmusic@gmail.com>
2017-07-22 17:59:00 +0100

    Item has name test passed

---
+++ shopping_basket_specs/ItemTest.java
@@ -0,0 +1,20 @@
+import shopping_basket.*;
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ItemTest {
+
+  Item item1;
+
+@Before
+public void before(){
+
+  item1 = new Item ("eggs",1.99, Type.FRESHFOOD);
+  
+  }
+
+@Test
+public void testItemHasName(){
+  assertEquals("eggs", item1.getName());
+  }
+}
